3d406688f8ef0f54b98d2a1e5ea71cab9925e4fb
Pierre-Benoît Querton <pb@querton.com>
2024-09-29 20:38:28 +0200

    core: update server

--- src/server.js
+++ src/server.js
@@ -1,30 +1,30 @@
-import { Server, Origins } from 'boardgame.io/server';
-import path from 'path';
-import serve from 'koa-static';
-import { nanoid } from 'nanoid';
-import BlancMangerQQ from './game/game';
-import answers from './data/answers.json';
+import { Server, Origins } from "boardgame.io/server";
+import path from "path";
+import serve from "koa-static";
+import { nanoid } from "nanoid";
+import BlancMangerQQ from "./app/game/game";
+import answers from "./app/data/answers.json";
 
 const accentsMap = {
-  a: 'á|à|ã|â|À|Á|Ã|Â',
-  e: 'é|è|ê|É|È|Ê',
-  i: 'í|ì|î|Í|Ì|Î|ï',
-  o: 'ó|ò|ô|õ|Ó|Ò|Ô|Õ',
-  u: 'ú|ù|û|ü|Ú|Ù|Û|Ü',
-  c: 'ç|Ç',
-  n: 'ñ|Ñ',
+  a: "á|à|ã|â|À|Á|Ã|Â",
+  e: "é|è|ê|É|È|Ê",
+  i: "í|ì|î|Í|Ì|Î|ï",
+  o: "ó|ò|ô|õ|Ó|Ò|Ô|Õ",
+  u: "ú|ù|û|ü|Ú|Ù|Û|Ü",
+  c: "ç|Ç",
+  n: "ñ|Ñ",
 };
 
 const slugify = (str) =>
   Object.keys(accentsMap).reduce(
-    (acc, cur) => acc.replace(new RegExp(accentsMap[cur], 'g'), cur),
-    str.replaceAll(/'/g, ''),
+    (acc, cur) => acc.replace(new RegExp(accentsMap[cur], "g"), cur),
+    str.replaceAll(/'/g, ""),
   );
 
 function camelize(str) {
   return str
     .replace(/(?:^\w|[A-Z]|\b\w)/g, (word) => word.toUpperCase())
-    .replace(/\s+/g, '');
+    .replace(/\s+/g, "");
 }
 
 const gameNames = [];
@@ -37,10 +37,10 @@ answers.forEach((anAnswer) => {
 const server = Server({
   games: [BlancMangerQQ],
   origins: [
-    'http://bmq.querton.com/',
+    "http://bmq.querton.com/",
     Origins.LOCALHOST_IN_DEVELOPMENT,
-    'http://192.168.1.39:3000',
-  ],  
+    "http://192.168.1.3:3000",
+  ],
   uuid: () =>
     `${gameNames[Math.floor(Math.random() * gameNames.length)]}-${nanoid(3)}`,
 });
@@ -48,14 +48,14 @@ const server = Server({
 const PORT = process.env.PORT || 8000;
 
 // Build path relative to the server.js file
-const frontEndAppBuildPath = path.resolve(__dirname, '../build');
+const frontEndAppBuildPath = path.resolve(__dirname, "../build");
 server.app.use(serve(frontEndAppBuildPath));
 
 server.run(PORT, () => {
   server.app.use(
     async (ctx, next) =>
       await serve(frontEndAppBuildPath)(
-        Object.assign(ctx, { path: 'index.html' }),
+        Object.assign(ctx, { path: "index.html" }),
         next,
       ),
   );
