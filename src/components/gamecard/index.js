daa9cb8b4d9b3cde1c3947069a299d2dc1cc7d8d
Pierre-Benoît Querton <pb@querton.com>
2024-09-30 08:38:22 +0200

    fix: remove bkgd image on question card

--- src/components/gamecard/index.js
+++ src/components/gamecard/index.js
@@ -39,12 +39,11 @@ export const GameCard = ({
 
   const cardBkgd = clsx(
     "card shadow-lg  w-36 sm:w-56 h-64 sm:h-80 flex flex-col items-center justify-start shadow-xl",
-    cardType === "questionCard" &&
-      "bg-accent-content text-white bg-[url('/images/splat2.svg')] bg-contain bg-right-bottom bg-no-repeat	",
+    cardType === "questionCard" && "bg-accent-content text-white ",
     cardType === "voteCard" && "bg-accent-content text-white",
     cardType === "answerCard" &&
       "bg-white text-black transition ease-in-out delay-0 hover:-translate-y-1 hover:scale-110  duration-100",
-    "bg-[url('/images/splat1.svg')] bg-contain bg-right-bottom bg-no-repeat	",
+    "bg-[url('/images/splat1.svg')] bg-contain bg-right-bottom bg-no-repeat",
   );
 
   const btnDispaly = clsx(
